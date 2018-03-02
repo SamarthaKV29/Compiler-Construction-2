5e5a0cd5ea508a0a4a0531d8c38038583a59a37a
SamarthaKV29 <samarthakv29@gmail.com>
2018-03-01 20:01:07 -0500

    upd

--- Compilers/pyCompilers-lesson/main.py
+++ Compilers/pyCompilers-lesson/main.py
@@ -2,6 +2,24 @@ import os
 import re
 import regutil
 
+class ParseTabRow:
+    def __init__(self, dT, vN, vVal, fN, fM):
+        self.dtype = dT
+        self.varName = vN
+        self.varVal = vVal
+        self.funcName = fN
+        self.funcMap = fM
+    def toList(self):
+        return list([
+            self.dtype,
+            self.varName,
+            self.varVal,
+            self.funcName,
+            self.funcMap,
+        ])
+
+parseTab = []
+
 regs = {
     "AX": [],
     "BX": [],
@@ -10,9 +28,8 @@ regs = {
 }
 
 functions = {
-    1: "int",
-    2: "print",
-    3: "string"
+    1: "var",
+    2: "disp",
 }
 
 
@@ -36,29 +53,46 @@ st = preprocess("test.my")
 
 def tokenize(stmts):
     for stmt in stmts:
-        if(re.match("var (\w)", stmt)):
-            val = re.findall("var (\w)", stmt)
+        if(re.match("var (.+)", stmt)):
+            val = re.findall("var (.+)", stmt)
             if(len(val) == 1):
                 val = val.pop()
-                print val
-        elif(re.match("disp\((\w)\)", stmt)):
-            val = re.findall("disp\((\w)\)", stmt)
-            if(len(val) == 1):
-                val = val.pop()
-                
+                if(type(val) != type(0)):
+                    if(str(val).find("=")):
+                        val = str(val).strip(" ")
+                        val = val.split("=")
+                        parserow = ParseTabRow("var", str(val[0]), int(val[1]), None, None)
+                        parseTab.append(parserow.toList())
+                else:
+                    print "type error in statement ", list(stmts).index(stmt)
 
+        elif re.match("disp\((.+)\)", stmt):
+            val = re.findall("disp\((.+)\)", stmt)
+            if(len(val) == 1):
+                val = val.pop()
+            try:
+                val = int(val)
+            except:
+                val = str(val)
+            parserow = None
+            if(type(val) == type(0)):
+                parserow = ParseTabRow("int", None, val, "disp", "disp")
+                
+            else:
+                if(str(val).startswith("\"") and str(val).endswith("\"")):
+                    parserow = ParseTabRow("string", None, val, "disp", "disp")
+                else:
+                    parserow = ParseTabRow("var", val, None, "disp", "disp")
+            parseTab.append(parserow.toList())
+        else:
+            print "Invalid syntax in statement", list(stmts).index(stmt)+1
+                
+def evaluate(pT):
+    for row in pT:
+        print row
         
 tokenize(st)
-print variables
-class ParseTabRow:
-    def __init__(self, dT, vN, vVal, fN, fM):
-        self.dtype = dT
-        self.varName = vN
-        self.varVal = vVal
-        self.funcName = fN
-        self.funcMap = fM
-
-parseTab = []
+evaluate(parseTab)
 
 
 
